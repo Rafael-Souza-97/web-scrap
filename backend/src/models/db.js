a8414a18abc458e0faa69933e764899bb30cac79
Rafael Souza <souza_rafael.97@outlook.com>
2023-04-16 00:32:45 -0300

    feat: created and connected to the database

---
+++ backend/src/models/db.js
@@ -0,0 +1,23 @@
+const dotenv = require('dotenv');
+const Sequelize = require('sequelize');
+dotenv.config();
+
+const connection = new Sequelize(
+  process.env.MYSQL_DB_NAME,
+  process.env.MYSQL_USER,
+  process.env.MYSQL_PASSWORD,
+  {
+    host: process.env.MYSQL_HOST || 'localhost',
+    dialect: 'mysql',
+    logging: false,
+  }
+);
+
+connection.authenticate()
+  .then(() => {
+    console.log('Conexão com o banco de dados realizada com sucesso!');
+  }).catch((error) => {
+    console.log('Erro: Conexão com o banco de dados não realizada com sucesso.', error);
+  });
+
+module.exports = connection;
--- backend/src/server.js
+++ backend/src/server.js
@@ -3,6 +3,8 @@ const app = require('./app');
 
 const port = process.env.API_PORT || 3001;
 
+const db = require('./models/db')
+
 app.get('/', (_request, response) => {
   response.send();
 });
